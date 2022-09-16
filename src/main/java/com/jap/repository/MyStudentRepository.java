2f6958baf6ca0d26dc2bb8e41430d93ba69a9641
M V K <krishna.mugala9@gmail.com>
2022-09-17 00:06:14 +0530

    added all methods and added logic to the methods

--- src/main/java/com/jap/repository/MyStudentRepository.java
+++ src/main/java/com/jap/repository/MyStudentRepository.java
@@ -10,9 +10,9 @@ package com.jap.repository;
 
 import com.jap.model.MyStudent;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyStudentRepository {
     public boolean add(Connection connection, MyStudent mystudents) throws SQLException {
@@ -27,12 +27,54 @@ public class MyStudentRepository {
             preparedStatement.setInt(2, (mystudents.getRollNumber()));
             preparedStatement.setString(3, (mystudents.getGrade()));
             preparedStatement.setInt(4, (mystudents.getTotalMarks()));
-
             // 4. execute the query
             numberOfRowsAffected = preparedStatement.executeUpdate();
         }
-
         return numberOfRowsAffected > 0;
     }
 
+    public List<MyStudent> getAll(Connection connection) throws SQLException {
+        // 1. write the query for selecting all the salesperson objects from the `sales_person` table
+        String readQuery = "SELECT * FROM `myschool`.`mystudent`;";
+        List<MyStudent> myStudent = new ArrayList<>();
+        // 2. create a statement object
+        try (Statement statement = connection.createStatement()) {
+            // 3. execute the query
+            ResultSet myStudentResultSet = statement.executeQuery(readQuery);
+            // 4. iterate over the result set and create a list of salesperson objects
+            while (myStudentResultSet.next()) {
+                // 5. fetch the values of the current row from the result set
+                String name = myStudentResultSet.getString("name");
+                int rollNumber = myStudentResultSet.getInt("rollNumber");
+                String city = myStudentResultSet.getString("grades");
+                int totalMarks = myStudentResultSet.getInt("totalMarks");
+                // 6. create a salesperson object using the values fetched from the result set
+                MyStudent myStudent1 = new MyStudent(name, rollNumber, city, totalMarks);
+                // 7. add the salesperson object to the list
+                myStudent.add(myStudent1);
+            }
+        }
+        return myStudent;
+    }
+
+    public boolean updateRollNumbers(Connection connection, int rollNumber, int totalMarks) throws SQLException {
+        String updateQuery = "UPDATE `myschool`.`mystudent` SET `rollNumber` = ? WHERE (`totalMarks` = ?);";
+        int numberOfRowsAffected;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(updateQuery)) {
+            preparedStatement.setInt(1, rollNumber);
+            preparedStatement.setInt(2, totalMarks);
+            numberOfRowsAffected = preparedStatement.executeUpdate();
+        }
+        return numberOfRowsAffected > 0;
+    }
+
+    public boolean deleteByRollNumber(Connection connection, int rollNumber) throws SQLException {
+        String deleteQuery = "DELETE FROM `myschool`.`mystudent` WHERE (`rollNumber` = ?);";
+        int numberOfRowsAffected;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(deleteQuery)) {
+            preparedStatement.setInt(1, rollNumber);
+            numberOfRowsAffected = preparedStatement.executeUpdate();
+        }
+        return numberOfRowsAffected > 0;
+    }
 }
