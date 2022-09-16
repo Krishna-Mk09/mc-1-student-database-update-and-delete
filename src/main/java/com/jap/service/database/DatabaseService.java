0dc45a6f25e4ef83fdb74997b248c560fe45307a
M V K <krishna.mugala9@gmail.com>
2022-09-16 22:36:32 +0530

    created getter method for connection in DatabaseService

--- src/main/java/com/jap/service/database/DatabaseService.java
+++ src/main/java/com/jap/service/database/DatabaseService.java
@@ -15,4 +15,8 @@ public class DatabaseService {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "Krishna@81433*";
     private Connection connection;
+
+    public Connection getConnection(){
+        return connection;
+    }
 }
