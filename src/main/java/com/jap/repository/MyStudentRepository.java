e93e96610e7d9fec65880d5d0e8328dd290a5345
M V K <krishna.mugala9@gmail.com>
2022-09-16 23:46:31 +0530

    created the insert method and added logic to the method

--- src/main/java/com/jap/repository/MyStudentRepository.java
+++ src/main/java/com/jap/repository/MyStudentRepository.java
@@ -8,6 +8,31 @@
 
 package com.jap.repository;
 
+import com.jap.model.MyStudent;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MyStudentRepository {
+    public boolean add(Connection connection, MyStudent mystudents) throws SQLException {
+        // 1. write the query for inserting a new salesperson object into the `sales_person` table
+        String insertQuery = "INSERT INTO `myschool`.`mystudent` " + "(`name`, `rollNumber`, `grade`,`totalMarks`) " + "VALUES (?, ?, ?, ?);";
+
+        // 2. create a statement object
+        int numberOfRowsAffected;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+            // 3. set the values of the query parameters
+            preparedStatement.setString(1, (mystudents.getName()));
+            preparedStatement.setInt(2, (mystudents.getRollNumber()));
+            preparedStatement.setString(3, (mystudents.getGrade()));
+            preparedStatement.setInt(4, (mystudents.getTotalMarks()));
+
+            // 4. execute the query
+            numberOfRowsAffected = preparedStatement.executeUpdate();
+        }
+
+        return numberOfRowsAffected > 0;
+    }
 
 }
