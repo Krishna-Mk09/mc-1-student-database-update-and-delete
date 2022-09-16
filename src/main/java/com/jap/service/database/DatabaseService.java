9ce7f24bdee1ecdbb9c3f5153b62a0529ec05906
M V K <krishna.mugala9@gmail.com>
2022-09-16 22:46:27 +0530

    created the connect method and connected the driver manager

--- src/main/java/com/jap/service/database/DatabaseService.java
+++ src/main/java/com/jap/service/database/DatabaseService.java
@@ -9,6 +9,8 @@
 package com.jap.service.database;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseService {
     private static final String URL = "jdbc:mysql://localhost:3306/myschool";
@@ -16,11 +18,20 @@ public class DatabaseService {
     private static final String PASSWORD = "Krishna@81433*";
     private Connection connection;
 
-    public Connection getConnection(){
+    public DatabaseService() {
+        this.connection = null;
+    }
+
+    public Connection getConnection() {
         return connection;
     }
 
-    public DatabaseService(Connection connection) {
-        this.connection = null;
+    public boolean connect() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        return connection != null;
     }
+
+
 }
