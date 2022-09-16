59cb8b8bca4c12cf73c177e48e19c21b5940419e
M V K <krishna.mugala9@gmail.com>
2022-09-16 22:49:44 +0530

    added java doc comments to database service class

--- src/main/java/com/jap/service/database/DatabaseService.java
+++ src/main/java/com/jap/service/database/DatabaseService.java
@@ -12,27 +12,47 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * It's a class that connects to the database and returns the connection object
+ */
 public class DatabaseService {
+    // It's a connection string that is used to connect to the database.
     private static final String URL = "jdbc:mysql://localhost:3306/myschool";
+    // It's a constant variable that is used to store the username of the database.
     private static final String USERNAME = "root";
+    // It's a constant variable that is used to store the password of the database.
     private static final String PASSWORD = "Krishna@81433*";
+    // It's a constructor that is used to initialize the connection object.
     private Connection connection;
 
     public DatabaseService() {
         this.connection = null;
     }
 
+    /**
+     * This function returns the connection.
+     *
+     * @return The connection object.
+     */
     public Connection getConnection() {
         return connection;
     }
 
+    /**
+     * > This function connects to the database using the URL, USERNAME, and PASSWORD variables
+     *
+     * @return A boolean value.
+     */
     public boolean connect() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-
         return connection != null;
     }
 
+    /**
+     * If the connection is not null, print a green message saying that the database is connected. Otherwise, print a red
+     * error message saying that the database is not connected
+     */
     public void printConnectStatus() {
         if (connection != null) {
             System.out.println("\u001B[32m Database is connected. \u001B[0m");
